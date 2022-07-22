8015b8b8e4544b90be48ea7faa57e68de8d7739e
Huy Cuong <phanhuycuong2909@gmail.com>
2022-07-22 09:34:57 +0700

    commit 22/7

---
+++ app/src/main/java/com/myapp/snackgame/ItemTruyen.java
@@ -0,0 +1,10 @@
+package com.myapp.snackgame;
+
+public class ItemTruyen {
+    String src;
+    String txt;
+    ItemTruyen(String txt , String src){
+        this.src = src;
+        this.txt = txt;
+    }
+}
